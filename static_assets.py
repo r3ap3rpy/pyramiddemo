8fcc1b021c4fc8910ce79dc00c9e53e3a49fcd21
Szabó Dániel Ernő <r3ap3rpy@localhost.localdomain>
2022-03-15 11:51:08 +0100

    Added stuff

--- static_assets.py
+++ static_assets.py
@@ -1,15 +1,14 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 
-
-
-
 if __name__ == '__main__':
 	with Configurator() as config:
-		config.add_route("hello","/{name}")
-		config.add_static_view(name="static",path="static")
+		config.add_route('hello','/{name}')
+		config.add_route('hello_json','/json/hello.json')
+		config.add_route('my_class','/classbased')
+		config.add_static_view(name='static',path='static')
 		config.include('pyramid_jinja2')
-		config.scan('template_jinja2_views_static')
+		config.scan('template_jinja2_views')
 		app = config.make_wsgi_app()
 	server = make_server('0.0.0.0',6543,app)
 	server.serve_forever()
--- template_jinja2_views.py
+++ template_jinja2_views.py
@@ -1,6 +1,19 @@
 from pyramid.view import view_config
 
-@view_config(route_name='home',renderer='hello_world.jinja2')
-def home_view(request):
+@view_config(route_name='hello',renderer='hello_world.jinja2')
+def hello_view(request):
 	return dict(name=request.matchdict['name'])
 
+@view_config(route_name='hello_json', renderer = 'json')
+def hello_json(request):
+	return [1,'a',1.1]
+
+@view_config(route_name='my_class')
+class HellClass:
+	def __init__(self,request):
+		self.name = request.matchdict['name']
+
+	@view_config(renderer='hello.jinja2')
+	def hello_view(self):
+		print("Simply view")
+		return dict()
